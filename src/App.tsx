bbdf25aff61f8735019cd243242513191a31d8dd
ChLalitha21 <chilaganilalitha1@gmail.com>
2025-07-29 23:38:03 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Contact from "./pages/Contact";
+
 import NotFound from "./pages/NotFound";
 import AcceleraatedEximSolution from "./pages/AcceleraatedEximSolution";
 import SAPManagedServices from "./pages/SAPManagedServices";
@@ -29,7 +29,7 @@ const App = () => (
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/contact" element={<Contact />} />
+
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
           <Route path="/sapmanagedservices" element={<SAPManagedServices />} />
