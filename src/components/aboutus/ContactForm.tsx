f3f63065d45564116073c8729347c67d3c8f03a1
Sriainfotech <ravirangari@sriainfotech.com>
2025-09-17 15:46:22 +0530

    sria-website with backend render url

--- src/components/aboutus/ContactForm.tsx
+++ src/components/aboutus/ContactForm.tsx
@@ -195,7 +195,7 @@ const ContactForm: React.FC = () => {
   setLoading(true);
   try {
     // Comment out the actual fetch call since there's no backend
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("https://sria-website-backend.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json"},
       body: JSON.stringify(formData),
